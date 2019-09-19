6f7af53c28712510a85d4ed8bd49bcad6fd4d675
Ben <bensiso1992@gmail.com>
2019-09-19 13:46:52 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -12,24 +12,19 @@ const config = {
     apiKey: "AIzaSyDZVbaOKb2xRhnEctxDbPcl_Em4GvXeBrM",
     authDomain: "tabfundme-d266e.firebaseapp.com",
     databaseURL: "https://tabfundme-d266e.firebaseio.com",
-    projectId: "tabfundme-d266e",
-    storageBucket: "tabfundme-d266e.appspot.com",
-    messagingSenderId: "394403115615",
-    
+    storageBucket: "tabfundme-d266e.appspot.com"
   };
 
+ firebase.initializeApp(config);
 
+  // Get a reference to the database service
+  var database = firebase.database();
 //create a functions to push
     function firebasePush(inputEmail,inputName,inputCommunityInfo) {
 
-
-        //prevents from braking
-        if (!firebase.apps.length) {
-            firebase.initializeApp(config);
-        }
         var rdm = Math.floor(Math.random() * 1000000); 
         //push itself
-        var mailsRef = firebase.database().ref('newLeadersRequests/' + rdm).push().set(
+        var mailsRef = database.ref('newLeadersRequests/' + rdm).push().set(
             {
                 mail: inputEmail.value,
                 name: inputName.value,
